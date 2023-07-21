b2f7f0e11eec324682254ca7e40a80f491702acc
ValentinAr7 <valio990210@gmail.com>
2023-07-21 10:36:47 +0300

    [14 Leetcode] Longest Common Prefix

--- 14. LongestCommonPrefix.js
+++ 14. LongestCommonPrefix.js
@@ -1,15 +1,14 @@
-var longestCommonPrefix = function(strs) {
-    
+var longestCommonPrefix = function (strs) {
+
     let prefix = strs[0]
 
-    for(let i = 0; i < strs.length; i++){
+    for (let i = 0; i < strs.length; i++) {
         while (strs[i].indexOf(prefix) !== 0) {
             prefix = prefix.substring(0, prefix.length - 1);
             if (!prefix) return "";
         }
     }
-    console.log(prefix);
-
+    return prefix
 };
 
-longestCommonPrefix(["flower","flow","flight"])
+longestCommonPrefix(["flower", "flow", "flight"])
